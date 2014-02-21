dd95a1fd790587c6e1de8dd9617ee84f894ed489
ulrich <ulrich@ulrich-Inspiron-N5010.(none)>
2014-02-21 13:46:43 +0100

    devise view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Confiture::Application.routes.draw do
+  devise_for :users
+  devise_for :installs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221105819) do
+ActiveRecord::Schema.define(version: 20140221124314) do
 
   create_table "ingredients", force: true do |t|
     t.string "name"
     t.float  "u_price"
   end
+
+  create_table "installs", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
+  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
 
   create_table "jars", force: true do |t|
     t.float  "price"
@@ -27,5 +45,23 @@ ActiveRecord::Schema.define(version: 20140221105819) do
     t.string "name"
     t.float  "u_price"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
