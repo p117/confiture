59ca53119a2fbcacbed136f573501f55f549004d
Ulrich <ulrich.palou@gmail.com>
2014-07-03 11:41:59 +0200

    create product historics

--- app/models/product_historic.rb
+++ app/models/product_historic.rb
@@ -1,6 +1,6 @@
-class ProductHistorics < ActiveRecord::Base
+class ProductHistoric < ActiveRecord::Base
 
-	validates :name , presence: true
+	validates :product_id , presence: true
   validates :u_price , presence: true
   validates :profit , presence: true
   
