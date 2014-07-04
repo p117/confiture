b1eca5c91052b0f0187a8466b64599413b2f2a04
Ulrich <ulrich.palou@gmail.com>
2014-07-04 11:45:34 +0200

    ingredient links on products

--- app/models/product_line.rb
+++ app/models/product_line.rb
@@ -2,7 +2,6 @@ class ProductLine < ActiveRecord::Base
   belongs_to :product, :dependent => :delete
   belongs_to :ingredient
 
-
   validates :amount , presence: true
   validates :product_id , presence: true
   validates :ingredient_id , presence: true
